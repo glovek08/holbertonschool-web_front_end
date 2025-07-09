cb97592cd3dcde58e1b26250097aeddd88ce5eaf
glovek08 <gbrielbbarn@gmail.com>
2025-07-09 09:32:42 -0300

    feat: project skeleton

---
+++ html_advanced/fucking_script.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Create all necessary files
+touch favicon.ico
+touch favicon.png
+touch article.html
+touch 5-index.html
+touch 6-index.html
+touch 7-index.html
+touch 8-index.html
+touch 9-index.html
+touch 10-index.html
+touch 11-styleguide.html
+touch 12-index.html
+touch 13-styleguide.html
+touch 14-index.html
+touch 15-index.html
+touch 16-index.html
+touch 17-index.html
+touch 18-index.html
+touch about.html
+touch latest_news.html
+touch contact.html
+touch 20-index.html
+touch 21-index.html
+touch 22-index.html
+touch 23-index.html
+touch 24-index.html
+touch 25-index.html
+touch 26-styleguide.html
+touch 27-index.html
+touch 28-styleguide.html
+touch 29-index.html
+touch 30-styleguide.html
+touch 31-index.html
+touch 32-styleguide.html
+touch 33-styleguide.html
+touch 34-styleguide.html
+touch 35-index.html
+touch 36-index.html
+
+# Create image directories and placeholder files
+mkdir -p images
+touch images/pic-work-01.jpg
+touch images/pic-work-02.jpg
+touch images/pic-work-03.jpg
+touch images/pic-about-us.jpg
+touch images/pic-blog-01.jpg
+touch images/pic-blog-02.jpg
+touch images/pic-blog-03.jpg
+touch images/pic-person-01.jpg
+touch images/pic-person-02.jpg
+touch images/pic-person-03.jpg
+touch images/pic-person-04.jpg
+touch logo-black.png
+
+echo "All specified files and directories have been created."
